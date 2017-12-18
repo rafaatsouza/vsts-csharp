83632001f19def178dc1834a7ac3183568ac8bb3
Name <souza.t.rafael@gmail.com>
2017-12-18 15:57:41 -0200

    Adds Infra.Provider project

---
+++ src/Infrastructure/Vsts.Infra.Provider/Factories/VstsProviderFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Vsts.Domain.Service.Factories;
+using Vsts.Domain.Service.Interfaces;
+
+namespace Vsts.Infra.Provider.Factories
+{
+    public class VstsProviderFactory : IVstsProviderFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+        
+        public VstsProviderFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IVstsProvider Create(string apiVersion, string account, string teamProject, string token)
+        {
+            return new VstsProvider(apiVersion, account, teamProject, token);
+        }
+    }
+}
