ac2b347d80937bcca82d761dcbce8a7fa778f5ae
Name <souza.t.rafael@gmail.com>
2017-12-18 15:21:45 -0200

    Adds Domain.Enum Proj

---
+++ src/Domain/Vsts.Domain.Enum/QueryResultType.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Vsts.Domain.Enum
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum QueryResultType
+    {
+        [EnumMember(Value = "workItem")]
+        WorkItem = 0
+    }
+}
