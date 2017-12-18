d53550b61cd429c2b7ecf52f91b0d357e6927f21
Name <souza.t.rafael@gmail.com>
2017-12-18 15:29:10 -0200

    Adds Domain.Service project

---
+++ src/Domain/Vsts.Domain.Service/Factories/IVstsProviderFactory.cs
@@ -0,0 +1,9 @@
+﻿using Vsts.Domain.Service.Interfaces;
+
+namespace Vsts.Domain.Service.Factories
+{
+    public interface IVstsProviderFactory
+    {
+        IVstsProvider Create(string apiVersion, string account, string teamProject, string token);
+    }
+}
---
+++ src/Domain/Vsts.Domain.Service/Interfaces/IVstsProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Vsts.Domain.Contract.Response;
+
+namespace Vsts.Domain.Service.Interfaces
+{
+    public interface IVstsProvider
+    {
+        Task<List<WorkItem>> GetWorkItemAsync(int[] workItemIds, string[] fields);
+
+        Task<WorkItemQueryResponse> GetWorkItemIdAsync(string where = null);
+    }
+}
